fd68eb3cfce77fba007b0b49b1adab76844988d3
Karolis <karolismarock@gmail.com>
2019-12-13 10:44:33 +0200

    moving player ship #3

--- js/game.js
+++ js/game.js
@@ -2,9 +2,12 @@ import Player from "./spaceShip.js";
 
 class ShooterGame {
     constructor (target, background) {
+        this.game;
+        this.dt = 0;
         this.DOM = document.querySelector(target);
         this.player = new Player();
         this.background = background;
+        
        
         this.init();
         this.renderBackground();
@@ -13,12 +16,28 @@ class ShooterGame {
         init() {
             this.DOM.classList.add('game');
             this.player.renderShip(this.DOM);
+            this.game = window.requestAnimationFrame(() => {
+                this.start()
+            })
         } 
 
         renderBackground() {
         const background = document.createElement('div');
         background.classList.add('space');
         document.getElementById('game').appendChild(background); 
+        }  
+        
+        start() {
+            const now = Date.now();
+            this.dt = (now - this.gameTime) / 1000;
+            this.gameTime = now;
+            
+            this.player.move( this.dt );
+
+            
+           window.requestAnimationFrame(() => {
+                this.start()
+            })
         }
 } 
 
--- js/spaceShip.js
+++ js/spaceShip.js
@@ -1,29 +1,138 @@
 class Player {
-    constructor(shipColor) {
-        this.shipColor = shipColor || 'blue';
+    constructor() {
+        this.position;
+        this.screenSize = {width: 1365, height: 655};
+        this.shipSize = { width: 66, height: 50};
+        this.shipSpeed = 300;
         this.keyboard;
-        this.contorlKeys();
-        this.laserRate = 3;
+        this.keyboardPressed = {
+            up: false,
+            down: false,
+            right: false,
+            left: false
+        };
+        
+        this.init();
     }
-    
-        contorlKeys(){
-            const controls = [
-                { up:'w', down:'s', left:'a', right:'d', laser:'space'}
-            ];
 
-            this.keyboard = controls;
+    init() {
+        this.setInitialPosition();
+    }
+
+    setInitialPosition() {
+        const coordinates = {
+            top: (this.screenSize.height - this.shipSize.height)  / 2,
+            left: 50
+        }
+        this.position = coordinates;   
+    }
+
+    renderShip = (DOM) => {
+        const ship = `<img id="playerShip" class="ship"
+                        src="./img/player-ships/playerShip1_blue.png"
+                        style ="width: ${this.shipSize.width}px;
+                                height: ${this.shipSize.height}px;
+                                top: ${this.position.top}px;
+                                left: ${this.position.left}px">`;
+        DOM.innerHTML = ship;
+     
+        window.addEventListener('keydown', (e) => {
+            switch (e.keyCode) {
+                case 87:
+                    this.keyboardPressed.up = true;
+                    
+                    break;
+                case 83:
+                    this.keyboardPressed.down = true;
+
+                    break;
+                case 65:
+                    this.keyboardPressed.left = true;
+
+                    break;
+                case 68:
+                    this.keyboardPressed.right = true;
+
+                    break;
+                case 32:
+                    this.fire();
+                    break;
+            
+                default:
+                    break;
+            }
+        })
+
+        window.addEventListener('keyup', (e) => {
+            switch (e.keyCode) {
+                case 87:
+                    this.keyboardPressed.up = false;
+                    
+                    break;
+                case 83:
+                    this.keyboardPressed.down = false;
+
+                    break;
+                case 65:
+                    this.keyboardPressed.left = false;
+
+                    break;
+                case 68:
+                    this.keyboardPressed.right = false;
+
+                    break;
+                case 32:
+                    this.fire();
+                    break;
+            
+                default:
+                    break;
+            }
+        }) 
+    } 
+
+    
+
+    move = ( dt ) => {
+        if ( this.keyboardPressed.up ) {
+            this.position.top -= this.shipSpeed * dt;    
         }
 
-        renderShip = (DOM) => {
-            const ship = `<img id="playerShip" src="./img/player-ships/playerShip1_${this.shipColor}.png"></img>`;
-            DOM.innerHTML = ship;
-            console.log(ship);
-            
-        } 
+        if ( this.keyboardPressed.down ) {
+            this.position.top += this.shipSpeed * dt;
+        }
+
+        if ( this.keyboardPressed.left ) {
+            this.position.left -= this.shipSpeed * dt;   
+        }
         
-      
-       
+        if ( this.keyboardPressed.right ) {
+            this.position.left += this.shipSpeed * dt;
+        }
+        
+        
+        
+
+        // saugomes, jog neisvaziuotu is arenos ribu
+        if ( this.position.top < 10 )
+             this.position.top = 10;
+            
+        if ( this.position.top > this.screenSize.height - this.shipSize.height - 10 ) 
+            this.position.top = this.screenSize.height - this.shipSize.height - 10;
+
+        if ( this.position.left < 10 ) 
+            this.position.left = 10;
+
+        if ( this.position.left > this.screenSize.width / 2 - this.shipSize.width ) 
+            this.position.left = this.screenSize.width / 2 - this.shipSize.width ;
+            console.log(this.position.left);
+            
+
+        document.querySelector('.ship').style.top = this.position.top + 'px';
+        document.querySelector('.ship').style.left = this.position.left + 'px';
+    }
 }
+
 export default Player;
 
 
